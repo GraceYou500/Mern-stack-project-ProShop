2b3edad3131382900b774a8a344d57a99e2dacab
Grace You <graceyouya500@gmail.com>
2022-09-21 23:59:52 +1000

    compass

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express'; // like import in JS, this express is the syntax of Node JS.
 import dotenv from 'dotenv';
-import products from './data/products.js';
+import colors from 'colors';
+// import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -11,18 +16,13 @@ app.get('/', (req, res) => {
 }); // create a route, meaning if we get a GST request to slash, we want to run the function
 // get is a listener=> listen the request to the url
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 ); // listen on a port 5000
