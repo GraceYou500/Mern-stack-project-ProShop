92ebadc5670bee17eebeaea8dbd7af71b33a4880
Grace You <graceyouya500@gmail.com>
2022-09-29 16:21:27 +1000

    get data for cartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log('cart screen', params, location);
+
+  const productId = params.id;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  console.log('cart', cart, cartItems);
+  console.log('cartItems', cartItems);
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>Cart</div>;
 };
 
