6fcbafb6bb2c26dcae7b21ffdc171f20e3ed416c
Grace You <graceyouya500@gmail.com>
2022-10-06 17:08:32 +1100

    user auth backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,19 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  console.log('this1111111----------', this);
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  console.log('this================', this);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
