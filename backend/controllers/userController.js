c0a409b9591769a4b7187a6ee288e6c4dbd10c66
Grace You <graceyouya500@gmail.com>
2022-10-20 22:48:38 +1100

    add profile screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,6 +29,8 @@ const authUser = asyncHandler(async (req, res) => {
 // @route          Get  /api/users/profile
 // @access         Private
 const getUserProfile = asyncHandler(async (req, res) => {
+  console.log('getUserProfile................');
+
   const user = await User.findById(req.user._id);
 
   if (user) {
@@ -37,6 +39,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @description    Update user profile
+// @route          PUT  /api/users/profile
+// @access         Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
     });
   } else {
     res.status(404);
@@ -77,4 +107,4 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+export { authUser, registerUser, getUserProfile, updateUserProfile };
