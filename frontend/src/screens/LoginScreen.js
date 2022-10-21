0bda810104770830136060ef72623238571f7058
Grace You <graceyouya500@gmail.com>
2022-10-21 16:38:21 +1100

    add shipping page

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -15,7 +15,7 @@ const LoginScreen = () => {
   const dispatch = useDispatch();
   const userLogin = useSelector((state) => state.userLogin);
   const { loading, error, userInfo } = userLogin;
-  const redirect = location.search ? location.search.split('=')[1] : '/';
+  const redirect = location.search ? '/' + location.search.split('=')[1] : '/';
 
   console.log('redirect location..........', location.search);
 
