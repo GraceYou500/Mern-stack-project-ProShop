3c24255c25d0b6789fe32cbe6fbb06296f0ba855
Grace You <graceyouya500@gmail.com>
2022-10-27 23:57:02 +1100

    fix security

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -18,6 +18,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -63,6 +64,7 @@ export const logout = () => async (dispatch) => {
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
+  dispatch({ type: USER_LIST_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
