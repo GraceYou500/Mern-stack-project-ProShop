c902b7d460e83fe5ccc18564ccb8e3a6129833bf
Grace You <graceyouya500@gmail.com>
2022-11-03 21:54:52 +1100

    add pageination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,9 @@ import Product from '../models/productModel.js';
 // @route          GET  /api/products
 // @access         Public
 const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2;
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -13,8 +16,13 @@ const getProducts = asyncHandler(async (req, res) => {
         },
       }
     : {}; // /api/products?keyword=... query is sth after question mark
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+
+  const count = await Product.count({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @description    Fetch single product
