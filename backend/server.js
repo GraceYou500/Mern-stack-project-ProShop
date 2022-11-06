7c373c24edc226953cf72e98d4a9f7e21d21419c
Grace You <graceyouya500@gmail.com>
2022-11-06 22:44:45 +1100

    set for deploy

--- backend/server.js
+++ backend/server.js
@@ -22,12 +22,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json()); // can accept json data
 
-app.get('/', (req, res) => {
-  res.send('API is running...0000');
-  // send to client
-}); // create a route, meaning if we get a GST request to slash, we want to run the function
-// get is a listener=> listen the request to the url
-
 app.use('/api/products', productRoutes); // use same as listen
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -39,6 +33,21 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  // means when we deploy to the server
+  app.use(express.static(path.join(__dirname, '/frontend/build'))); // set the build folder as static folder so that we can access to and load the build-index.html directly
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  ); // if we are in production, we are going to go ahead and get any route that is not our API to point to the index.html
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...0000');
+    // send to client
+  }); // create a route, meaning if we get a GET request to slash, we want to run the function
+  // get is a listener=> listen the request to the url
+}
 
 app.use(notFound);
 app.use(errorHandler);
